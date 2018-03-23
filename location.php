eb265a92116ca2dcfc15bd94a40f9715457f7c8f
chiragk156 <chiragk156@gmail.com>
2018-03-23 22:38:33 +0530

    Location and City fetch

---
+++ location.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Location</title>
+	<script src="script.js" type="text/javascript"></script>
+</head>
+<body>
+	<p><button onclick="geoFindMe()">Show my location</button></p>
+	<div id="out"></div>
+</body>
+</html>
